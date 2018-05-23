4fcb53154e82fd7a26090dc1bef941b864044478
VIJEXA <nuzhdinvitja@gmail.com>
2018-05-23 12:04:01 +0300

    *check_daylight_saving() optimised

--- src/application.lua
+++ src/application.lua
@@ -6,29 +6,33 @@ lamp_pin = 2
 pwm_freq = 1000
 settings.toggle_time.on = settings.toggle_time.on.hour*60 + settings.toggle_time.on.min
 settings.toggle_time.off = settings.toggle_time.off.hour*60 + settings.toggle_time.off.min
+daylight_saving = nil
 current_time = {}
 
 -- checking if it is daylight saving time now
 function check_daylight_saving()
-  local months = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31}
+  if(daylight_saving == nil) then
+    local months = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31}
 
-  local days_start = 0
-  for i=1, settings.daylight_saving_period.start.month-1, 1 do
-    days_start = days_start + months[i]
-    if(i == 2) and (current_time.year % 4 == 0) then days_start = days_start + 1 end
+    local days_start = 0
+    for i=1, settings.daylight_saving_period.start.month-1, 1 do
+      days_start = days_start + months[i]
+      if(i == 2) and (current_time.year % 4 == 0) then days_start = days_start + 1 end
+    end
+    days_start = days_start + settings.daylight_saving_period.start.month_day
+
+    local days_end = 0
+    for i=1, settings.daylight_saving_period._end.month-1, 1 do
+      days_end = days_end + months[i]
+      if(i == 2) and (current_time.year % 4 == 0) then days_end = days_end + 1 end
+    end
+    days_end = days_end + settings.daylight_saving_period._end.month_day
+
+    print("saving start "..days_start.." end "..days_end)
+    print((current_time.yday>days_start) and (current_time.yday<days_end))
+    daylight_saving = (current_time.yday>days_start) and (current_time.yday<days_end)
   end
-  days_start = days_start + settings.daylight_saving_period.start.month_day
-
-  local days_end = 0
-  for i=1, settings.daylight_saving_period._end.month-1, 1 do
-    days_end = days_end + months[i]
-    if(i == 2) and (current_time.year % 4 == 0) then days_end = days_end + 1 end
-  end
-  days_end = days_end + settings.daylight_saving_period._end.month_day
-
-  print("saving start "..days_start.." end "..days_end)
-  print((current_time.yday>days_start) and (current_time.yday<days_end))
-  return (current_time.yday>days_start) and (current_time.yday<days_end)
+  return daylight_saving
 end
 
 function format_time() 
