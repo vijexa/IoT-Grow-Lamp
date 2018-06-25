aa26f7a2acf15de8e0b83847d1f9058c54800fed
VIJEXA <nuzhdinvitja@gmail.com>
2018-06-25 23:45:02 +0300

    * log file renamed

--- src/init_wifi.lua
+++ src/init_wifi.lua
@@ -10,11 +10,11 @@ local print = nil
 if(settings.write_log) then
   print = function(input)
     _print(input)
-    local stat = file.stat("log")
+    local stat = file.stat(".log")
     if (stat.size > 3000000) then 
-      file.remove("log")
+      file.remove(".log")
     end
-    log_file = file.open("log", "a+")
+    log_file = file.open(".log", "a+")
     if (log_file) then 
       log_file:writeline(input)
       log_file:close()
