65cfde61314e87b1c29585f9d9eeebf8923cf5c0
VIJEXA <nuzhdinvitja@gmail.com>
2018-06-30 18:56:18 +0300

    * format_time moved to other file

--- src/application.lua
+++ src/application.lua
@@ -16,23 +16,7 @@ local print = shared_obj.print
 
 if (settings.daylight_saving) then dofile("check_daylight_saving.lua") end
 
-function format_time() 
-	current_time = rtctime.epoch2cal(rtctime.get())
-	current_time.hour = current_time.hour + settings.GMT
-	if (settings.daylight_saving) and (check_daylight_saving()) then current_time.hour = current_time.hour + 1 end
-	if (current_time.hour >= 24) then current_time.hour = current_time.hour - 24 end
-	print(current_time.hour..":"..current_time.min)
-	current_time.time = current_time.hour * 60 + current_time.min
-	-- debugging things
-	if(settings.day_pass.pass) then 
-		print("real  "..current_time.time)
-		while (current_time.time >= settings.toggle_time.off + 3) do
-			current_time.time = current_time.time - settings.day_pass.back_in_time;
-		end
-	end
-	print("formatted  "..current_time.time)
-	print("on "..settings.toggle_time.on.." off "..settings.toggle_time.off)
-end
+dofile("format_time.lua")
 
 function DIV(a, b)
 	return (a - a % b) / b
