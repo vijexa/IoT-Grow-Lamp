5732d0cc6cee3658df61e08ce62d009f450fd4c6
VIJEXA <nuzhdinvitja@gmail.com>
2018-06-30 20:36:56 +0300

    * if connection to sunrise-sunset api was unsuccessful, use saved data from yesterday

--- src/use_sun_times.lua
+++ src/use_sun_times.lua
@@ -2,8 +2,8 @@ if (settings.check_heap) then print("\n////use_sun_times.lua heap "..node.heap()
 
 print("current time "..current_time.time)
 local FNAME = "sun_times.json"
-file.open(FNAME, "r")
-local sun_times = file.read()
+local sun_times_file = file.open(FNAME, "r")
+local sun_times = sun_times_file.read()
 file.close()
 sun_times = sjson.decode(sun_times)
 if(current_time.yday == sun_times.day) then
@@ -19,21 +19,27 @@ else
     url = url..tostring(settings.coordinates.longitude)
     print(url)
     http.get(url, nil, function(code, raw_data)
-        print(raw_data)
-        raw_data = sjson.decode(raw_data)
-        local data = {}
-        local function convert(raw_time)
-            local val = tonumber(string.match(raw_time, "(.-):")) + settings.GMT + (check_daylight_saving() and 1 or 0)
-            val = (val + (string.match(raw_time, " (.+)") == "PM" and 12 or 0)) * 60
-            return val + tonumber(string.match(raw_time, ":(.-):"))
+        if (code ~= -1) then
+            print(raw_data)
+            raw_data = sjson.decode(raw_data)
+            local data = {}
+            local function convert(raw_time)
+                local val = tonumber(string.match(raw_time, "(.-):")) + settings.GMT + (check_daylight_saving() and 1 or 0)
+                val = (val + (string.match(raw_time, " (.+)") == "PM" and 12 or 0)) * 60
+                return val + tonumber(string.match(raw_time, ":(.-):"))
+            end
+            data.sunrise = convert(raw_data.results.sunrise)
+            data.sunset = convert(raw_data.results.sunset)
+            data.day = current_time.yday
+            raw_data = sjson.encode(data)
+            file.open(FNAME, "w")
+            file.write(raw_data)
+            file.close()
+            
+        else
+            -- if connection to sunrise-sunset api was unsuccessful, use saved data from yesterday
+            data = sun_times
         end
-        data.sunrise = convert(raw_data.results.sunrise)
-        data.sunset = convert(raw_data.results.sunset)
-        data.day = current_time.yday
-        raw_data = sjson.encode(data)
-        file.open(FNAME, "w")
-        file.write(raw_data)
-        file.close()
         settings.toggle_time.on = data.sunrise
         -- adding settings.fade_time to ensure that lamp will start to turning off on sunset time 
         settings.toggle_time.off = data.sunset + (settings.fade and settings.fade_time or 0) 
